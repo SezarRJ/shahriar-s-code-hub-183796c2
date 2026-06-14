be3752418c2c3d5024b1f3649b7cae7a0c74c8b8
SezarRJ <Rafi6630@gmail.com>
2026-06-15 02:14:00 +0300

    Update from Blink - 2026-06-14T23:13:47.827Z

---
+++ app.config.timestamp_1781478788047.js
@@ -0,0 +1,8 @@
+// app.config.ts
+import { defineConfig } from "@tanstack/start/config";
+var app_config_default = defineConfig({
+  // Configuration for TanStack Start
+});
+export {
+  app_config_default as default
+};
